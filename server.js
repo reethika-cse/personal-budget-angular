fbf4a833fb23fe063f0c7d053c2e8c0c88aa05b1
reethika-cse <reethikareddy19965@gmail.com>
2024-02-04 07:32:09 -0500

    Added server, axios and ChartsJs

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = require('./budget.json');
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.get('/hello', (req,res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
